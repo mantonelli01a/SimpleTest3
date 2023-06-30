f3e8a6f2650ba15b8c80bd420c34ea8230f7f4ef
mantonelli01a <mike@mantonelli.plus.com>
2023-06-30 12:17:23 +0100

    Initial Commit

---
+++ SimpleTest3/SimpleTest3App.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTest3App.swift
+//  SimpleTest3
+//
+//  Created by Michael Antonelli on 30/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct SimpleTest3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
